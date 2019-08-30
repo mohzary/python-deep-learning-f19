4e880ea1fad1dd8a2392596348610bbd32ca8d71
MUHAMMAD MOHZARY <mmohzary@kent.edu>
2019-08-30 11:38:18 -0500

    Delete replace-python.py

--- lab 1/replace-python.py
+++
@@ -1,6 +0,0 @@
-# Muhammad Mohzary ICP 1
-# Python script to accepts a sentence and replace each occurrence of ‘python’ with ‘pythons’
-
-# To ask user to enter a sentence
-userSentence = str(input("Hi, what do you think about python? "))
-print(userSentence.replace("python", "pythons"))
