7a8d03956718db1402efe4a458beb40d62744f52
Muhammad Mohzary <mmohzary@kent.edu>
2019-08-30 15:53:53 -0500

    weights program is ready

---
+++ ICP 2/weights.py
@@ -0,0 +1,25 @@
+#ICP 2
+#Muhammad Mohzary
+
+# A program which reads weights (lbs.) of N students into a list and convert these weights to kilograms in a separate list using
+
+# the convert function to convert ibs to kgs
+def convertInput(weights):
+    weightskg = []
+    for w in weights:
+        weightskg.append(w / 2.205)
+    return(weightskg)
+
+
+#T ask user to enter the number of students
+studentsN = int(input("Please enter the number of students: "))
+# to initi the list
+weightslbsList = []
+
+# Using loop to enter student weights
+for x in range(studentsN):
+    w = float(input("Please enter student number " + str(x + 1) +" weight: "))
+    weightslbsList.append(w)
+print(weightslbsList)
+# To call the convert function
+print(convertInput(weightslbsList))
